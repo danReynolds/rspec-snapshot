22d1ca744b9cfce055658e3443b1ac10731d76f2
Dan Reynolds <me@danreynolds.ca>
2018-01-20 16:29:17 -0800

    fix rspec typo

--- lib/rspec/snapshot/matchers/match_snapshot.rb
+++ lib/rspec/snapshot/matchers/match_snapshot.rb
@@ -13,7 +13,7 @@ module RSpec
           @actual = actual
           filename = "#{@snapshot_name}.snap"
           snap_path = File.join(snapshot_dir, filename)
-          update_snapshots = Rspec.configuration.update_snapshots
+          update_snapshots = RSpec.configuration.update_snapshots
           FileUtils.mkdir_p(File.dirname(snap_path)) unless Dir.exist?(File.dirname(snap_path))
           if File.exist?(snap_path) && !update_snapshots
             file = File.new(snap_path)
