b8d2287fbe992ead5c241638d3238b409f75fd9e
Dan Reynolds <me@danreynolds.ca>
2018-01-20 16:23:00 -0800

    add setting for updating

--- lib/rspec/snapshot/configuration.rb
+++ lib/rspec/snapshot/configuration.rb
@@ -4,6 +4,7 @@ module RSpec
 
     def self.initialize_configuration(config)
       config.add_setting :snapshot_dir, :default => :relative
+      config.add_setting :update_snapshots, :default => false
     end
 
     initialize_configuration RSpec.configuration
